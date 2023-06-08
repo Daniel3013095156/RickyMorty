2df31c5778a7433c92964c3d09f613cbb7120378
Daniel3013095156 <daniel3013095156@gmail.com>
2023-06-08 17:06:23 -0500

    second commit

--- src/util/components/Location.jsx
+++ src/util/components/Location.jsx
@@ -1,41 +1,43 @@
-import axios from "axios"
-const Location = ({location, setlocation}) => {
+// Location.jsx
+import axios from "axios";
 
-  const handleSubmit= (e) => {
-    e.preventDefault()
-    const newLocation= e.target.newLocation.value
+const Location = ({ location, setlocation }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newLocation = e.target.newLocation.value;
 
-    const URL = `https://rickandmortyapi.com/api/location/${newLocation}`
+    const URL = `https://rickandmortyapi.com/api/location/${newLocation}`;
 
     axios.get(URL)
-      .then(({data}) => setlocation(data))
-      .catch((err) => console.log(err))
+      .then(({ data }) => setlocation(data))
+      .catch((err) => console.log(err));
   }
 
   return (
-    <section>
-      {/*Input de busqueda*/}
-      <form onSubmit={handleSubmit}>
-        "
-        <input id="newLocation" className= "text-black" placeholder="Type a locatión Id..." type="text" />
-        <button>Search</button> <i className='bx bx-search-alt'></i>
-      </form>
-      {/*Info Location*/}
-    
+    <section className="bg-space min-h-screen text-white">
+      <header className="text-center py-8">
+        <h1 className="text-3xl font-bold">Rick and Morty Location Finder</h1>
+      </header>
 
-    <section>
-        <h2>{location?.name}</h2>
-        <ul>
-            <li>{location?.type}</li>
-            <li>{location?.dimension} </li>
-            <li>{location?.residents.length}</li>
-        </ul>
+      <section className="container mx-auto">
+        <form onSubmit={handleSubmit} className="flex justify-center mb-8">
+          <input id="newLocation" className="text-black bg-gray-200 rounded px-4 py-2 mr-2" placeholder="Type a location ID..." type="text" />
+          <button className="bg-blue-500 hover:bg-blue-600 text-white rounded px-4 py-2">Search</button>
+        </form>
+
+        {location && (
+          <div className="text-center">
+            <h2 className="text-2xl font-bold mb-4">{location.name}</h2>
+            <ul>
+              <li>Type: {location.type}</li>
+              <li>Dimension: {location.dimension}</li>
+              <li>Number of Residents: {location.residents.length}</li>
+            </ul>
+          </div>
+        )}
       </section>
-    <section>
-
-        </section>
     </section>
-  )
+  );
 }
 
-export default Location
+export default Location;
--- src/util/components/Resident.jsx
+++ src/util/components/Resident.jsx
@@ -1,46 +1,53 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+// Resident.jsx
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const Resident = ({residentUrl}) => {
-const [residentInfo, setresidentInfo] = useState(null)
+const Resident = ({ residentUrl }) => {
+  const [residentInfo, setResidentInfo] = useState(null);
 
   const statusStyle = {
-    "Alive":"bg-green-500",
-    "Dead":"bg-red-500",
-    "unknown":"bg-gray-400"
-  }
+    Alive: "bg-green-500",
+    Dead: "bg-red-500",
+    unknown: "bg-gray-400",
+  };
 
-    useEffect (()=> {
-        axios.get(residentUrl)
-        .then(({data}) => setresidentInfo(data))
-        .catch((err) => console.log(err))
-    }, [])
+  useEffect(() => {
+    axios
+      .get(residentUrl)
+      .then(({ data }) => setResidentInfo(data))
+      .catch((err) => console.log(err));
+  }, []);
 
-return (
-    
-    <article>
-<div className="relative">
-  <img src={residentInfo?.image} alt="" />
-  <div className="flex items-center gap-2 absolute bottom-3 left-1/2 -translate-x-1/2">
-    <div className={`h-6 w-6 ${statusStyle [residentInfo?.status]} rounded-full`}></div>
-    <div className="bg-black bg-opacity-50 text-white border-1 border-green-500 px-6 py-1 shadow-lg">
-      {residentInfo ?.status}
-    </div>
-  </div>
-</div>
+  return (
+    <article className="rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
+      <div className="relative">
+        <img src={residentInfo?.image} alt={residentInfo?.name} className="w-full" />
+        <div className="flex items-center gap-2 absolute bottom-3 left-1/2 -translate-x-1/2">
+          <div
+            className={`h-6 w-6 ${statusStyle[residentInfo?.status]} rounded-full shadow-xl animate-pulse`}
+          ></div>
+          <div className="bg-black bg-opacity-50 text-white border-1 border-green-500 px-6 py-1 shadow-lg">
+            {residentInfo?.status}
+          </div>
+        </div>
+      </div>
 
-
-
-    <section>
-        <h4>{residentInfo?.name}</h4>
-        <ul>
-            <li>Species: <span>{residentInfo?.species}</span></li>
-            <li>Origin: <span>{residentInfo?.origin.name}</span></li>
-            <li>Times apper: <span>{residentInfo?.episode.length}</span></li>
+      <section className="p-4">
+        <h4 className="text-xl font-bold">{residentInfo?.name}</h4>
+        <ul className="mt-2">
+          <li>
+            <span className="font-semibold">Species:</span> {residentInfo?.species}
+          </li>
+          <li>
+            <span className="font-semibold">Origin:</span> {residentInfo?.origin.name}
+          </li>
+          <li>
+            <span className="font-semibold">Times appeared:</span> {residentInfo?.episode.length}
+          </li>
         </ul>
-    </section>
-</article>
-)
-}
+      </section>
+    </article>
+  );
+};
 
-export default Resident
+export default Resident;
